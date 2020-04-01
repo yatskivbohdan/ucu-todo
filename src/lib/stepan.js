75c1dd2fe5ce5cf5f86acf2452265e96076c31d2
yatskivbohdan <44812624+yatskivbohdan@users.noreply.github.com>
2020-04-02 01:38:26 +0300

    Finished todos

--- src/lib/stepan.js
+++ src/lib/stepan.js
@@ -1,8 +1,10 @@
-
 export default class Stepan {
   static createElement(element, parent, attributes = {}) {
     // TODO: check if this is a valid tag name
+
     const newElement = document.createElement(element);
+    if (newElement.toString() === "[object HTMLUnknownElement]")
+      throw new StepanError("Tag name is invalid");
 
     const { innerHTML, innerText } = attributes;
 
@@ -28,9 +30,21 @@ export default class Stepan {
       // TODO: 1. Create StepanError class to define all framework errors
       //       2. throw an error if parent is null or undefined, or if it's not a valid DOM object
 
+      if (!parent || typeof parent === 'undefined' || !(parent instanceof Element))
+        throw new StepanError("Parent is invalid");
+
       this.parent = parent;
     }
 
     // TODO (Bonus): Ensure that every component returns a top-level root element
   }
 }
+
+
+class StepanError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "StepanError";
+  }
+}
+
